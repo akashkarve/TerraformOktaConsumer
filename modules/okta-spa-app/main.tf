ddf605fa203d69338287623ebe06c497c8b9fd2d
akashkarve <akash.karve@wellsky.com>
2021-07-16 20:45:46 -0500

    added spa app module
    added spa app module

---
+++ modules/okta-spa-app/main.tf
@@ -0,0 +1,17 @@
+
+# SPA with PKCE flow
+resource "okta_app_oauth" "okta-spa-app" {
+    type = "browser"
+    label = var.app_name
+    status = var.app_status    
+    token_endpoint_auth_method = "none"
+    auto_key_rotation = false
+
+    redirect_uris = var.app_redirect_uris
+    post_logout_redirect_uris = var.app_post_logout_redirect_uris
+
+    grant_types = var.app_grant_types
+    response_types = ["code"]
+
+    omit_secret = true
+}
